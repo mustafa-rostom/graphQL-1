2dcbe464917a3973696c29b3cc094039474da55c
Mustafa Rostom <mutafarostom@gmail.com>
2022-02-22 17:19:07 +0200

    task

--- server.js
+++ server.js
@@ -63,78 +63,120 @@ const schema = buildSchema(`
 
 
 const userMutations = {
-  createUser: async ({
-    userData: {
-      username, password, firstName, lastName, age,
+    createUser: async({
+        userData: {
+            username,
+            password,
+            firstName,
+            lastName,
+            age,
+        },
+    }) => {
+        const user = new User({
+            username,
+            password,
+            firstName,
+            lastName,
+            age,
+        });
+        await user.save();
+        return {
+            firstName,
+            lastName,
+            age,
+        };
     },
-  }) => {
-    const user = new User({
-      username,
-      password,
-      firstName,
-      lastName,
-      age,
-    });
-    await user.save();
-    return {
-      firstName,
-      lastName,
-      age,
-    };
-  },
-  loginUser: async ({ username, password }) => {
-    const user = await User.findOne({ username });
-    if (!user) return { error: 'Login failed' };
-    if (user.password !== password) return { error: 'Login failed' };
-    const token = jwt.sign({ userId: user.id }, jwtSecret);
-    return { token };
-  },
+    loginUser: async({ username, password }) => {
+        const user = await User.findOne({ username });
+        if (!user) return { error: 'Login failed' };
+        if (user.password !== password) return { error: 'Login failed' };
+        const token = jwt.sign({ userId: user.id }, jwtSecret);
+        return { token };
+    },
 };
 
-const auth = async (token) => {
-  try {
-    const payload = jwt.verify(token, jwtSecret);
-    const user = User.findById(payload.userId);
-    return user;
-  } catch (error) {
-    return null;
-  }
+const auth = async(token) => {
+    try {
+        const payload = jwt.verify(token, jwtSecret);
+        const user = User.findById(payload.userId);
+        return user;
+    } catch (error) {
+        return null;
+    }
 };
 
 
 const postsMutation = {
-  postCreate: async ({ content, token }) => {
-    const user = await auth(token);
-    if (!user) return 'Authentication error';
-    const userId = user.id;
-    const post = new Post({ userId, content });
-    await post.save();
-    return 'Success';
-  },
+    postCreate: async({ content, token }) => {
+        const user = await auth(token);
+        if (!user) return 'Authentication error';
+        const userId = user.id;
+        const post = new Post({ userId, content });
+        await post.save();
+        return 'Success';
+    },
 };
 
 
 const postsQuery = {
-  getMyPosts: async ({ token }) => {
-    const user = await auth(token);
-    if (!user) return 'Authentication error';
-    const userId = user.id;
-    const posts = await Post.find({ userId });
-    return posts.map(p => ({ ...p, user }));
-  },
+    getMyPosts: async({ token }) => {
+        const user = await auth(token);
+        if (!user) return 'Authentication error';
+        const userId = user.id;
+        const posts = await Post.find({ userId });
+        return posts.map(p => ({...p, user }));
+    },
 
-  getAllPosts: async () => {
-    const posts = await Post.find({}).populate('userId')
-    return posts.map(p => ({ ...p.toJSON(), user: p.userId }))
-  },
+    getAllPosts: async() => {
+        const posts = await Post.find({}).populate('userId')
+        return posts.map(p => ({...p.toJSON(), user: p.userId }))
+    },
+};
+//edit post
+postEdit: async({ token, postID, newContent }) => {
+        const user = await auth(token);
+        if (!user) return "Authentication error";
+        const userId = user.id;
+        const post = await Post.findOneAndUpdate({ userId, _id: postID }, { content: newContent });
+        await post.save();
+        return "Successful Editing";
+    }
+    //delete post
+postDelete: async({ token, postID }) => {
+    const user = await auth(token);
+    if (!user) return "Authentication error";
+    const userId = user.id;
+    await Post.findOneAndDelete({ userId, _id: postID });
+    return "Deleted Successfully";
+}
+const commentMutations = {
+    createComment: async({ token, postID, commentContent }) => {
+        const user = await auth(token);
+        if (!user) return "Authentication error";
+        const userId = user.id;
+        const post = await Post.find({ userId, _id: postID });
+        post[0].comments.push({ commentContent });
+        await post[0].save();
+        return `Comment created successfully, there are ${post[0].comments.length} comments!`;
+    }
+};
+const commentsQuery = {
+    getComments: async({ token, postID }) => {
+        const user = await auth(token);
+        if (!user) return "Authentication error";
+        const userId = user.id;
+        const post = await Post.find({ userId, _id: postID });
+        return post[0].comments;
+    }
 };
 
-
 const rootValue = {
-  ...userMutations,
-  ...postsMutation,
-  ...postsQuery,
-  hello: () => 'Hello world',
+    ...userMutations,
+    ...postsMutation,
+    ...postsQuery,
+    ...commentMutations,
+    ...commentsQuery,
+    hello: () => 'Hello world',
 };
 
 const app = express();
@@ -142,5 +184,5 @@ const app = express();
 app.use('/graph', graphqlHTTP({ schema, rootValue, graphiql: true }));
 
 app.listen(5000, () => {
-  console.log('Server is runing');
+    console.log('Server is runing');
 });
